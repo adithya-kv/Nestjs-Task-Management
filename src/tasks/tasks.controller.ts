0fd36dc1efacc143bf0e39ab1276367d86e7c2e9
adithya-kv <adithya@keyvalue.systems>
2021-10-13 15:04:39 +0530

    Create Task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { title } from 'process';
 
 @Controller('tasks')
 export class TasksController {
@@ -9,5 +10,12 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body('title') title,
+  @Body('description') description
+  ): Task {
+    return this.tasksService.createTask(title, description);
   }
 }
