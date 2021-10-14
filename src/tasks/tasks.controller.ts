aaa3a741d41222dc1094a96f16bfcf9fd71f39c6
adithya-kv <adithya@keyvalue.systems>
2021-10-14 09:46:17 +0530

    Add search tasks using filter

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,15 +1,20 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { getTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() filterDto: getTasksFilterDto): Task[] {
+    if (filterDto) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
   }
 
   @Post()
@@ -17,17 +22,17 @@ export class TasksController {
     return this.tasksService.createTask(createTaskDto);
   }
 
-  @Get('/:id')
+  @Get(':id')
   getTaskById(@Param('id') id: string): Task {
     return this.tasksService.getTaskById(id);
   }
 
-  @Delete('/:id')
+  @Delete(':id')
   deleteTask(@Param('id') id: string): void {
     return this.tasksService.deleteTask(id);
   }
 
-  @Put('/:id/status')
+  @Put(':id/status')
   updateTaskStatus(
     @Param('id') id: string,
     @Body('status') status: TaskStatus
