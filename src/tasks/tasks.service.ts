1968192fe4710750557ee744ce318eb2e35610fb
adithya-kv <adithya@keyvalue.systems>
2021-10-14 13:24:23 +0530

    Add notFoundException for getTaskById

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -42,11 +42,17 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find((task) => task.id === id);
+        const task = this.tasks.find((task) => task.id === id);
+        if (!task) {
+            throw new NotFoundException(`Task with "${id}" not found`);
+        } else {
+            return task;
+        }
     }
 
     deleteTask(id: string): void {
-        this.tasks = this.tasks.filter((task) => task.id !== id);
+        const found = this.getTaskById(id);
+        this.tasks = this.tasks.filter((task) => task.id !== found.id);
     }
 
     updateTaskStatus(id: string, status: TaskStatus): Task {
